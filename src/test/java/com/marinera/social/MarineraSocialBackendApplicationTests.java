3a364c34d97f884ba2297d445ae1b199012b2239
Alejandro Jacinto <jacintoalejandroj722@gmail.com>
2025-07-05 13:51:22 -0500

    feat: proyecto Spring Boot inicial desde Spring Initializr

---
+++ src/test/java/com/marinera/social/MarineraSocialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marinera.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarineraSocialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
